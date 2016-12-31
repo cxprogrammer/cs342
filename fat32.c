8c7b34da575f973155810362e396ec19c0f3d638
Live session user <gizemcaylak@gmail.com>
2016-12-31 07:44:37 +0000

    update

--- fat32.c
+++ fat32.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <time.h>
 #include <linux/msdos_fs.h>
+#include <ctype.h>
 
 #define DIRECTORYENTITYSIZE 	32	//bytes
 #define SECTORSPERCLUSTER 	8
@@ -26,6 +27,21 @@ int get_sector (unsigned char *buf, int snum)
 	offset = snum * SECTORSIZE; 
 	lseek (disk_fd, offset, SEEK_SET); 
 	n  = read (disk_fd, buf, SECTORSIZE); 
+	if (n == SECTORSIZE) 
+		return (0); 
+	else {
+		printf ("sector number %d invalid or read error.\n", snum); 
+		exit (1); 
+	}
+}
+
+int set_sector (unsigned char *buf, int snum)
+{
+	off_t offset; 
+	int n; 
+	offset = snum * SECTORSIZE; 
+	lseek (disk_fd, offset, SEEK_SET); 
+	n  = write (disk_fd, buf, SECTORSIZE); 
 	if (n == SECTORSIZE) 
 		return (0); 
 	else {
@@ -82,7 +98,7 @@ void print_rootdir() {
 			}
 			mde = (struct msdos_dir_entry *) entity;
 			if(entity[0] == 0)
-				break;
+				continue;
 			/* name of the entity*/ 
 			unsigned char* name = mde->name;
 			/* attribute */
@@ -119,7 +135,7 @@ void print_blocks_allocated(char* filename) {
 	/* calculate beginning of the root folder boot sec +reserved secs + 2*fat secs*/
 	unsigned int root_folder_begin_sec = 32 + 2*sectors_per_fat_length;
 	
-	printf("Blocks allocated:\n");
+	printf("\nBlocks allocated:\n");
 	int count = 0;
 	int i;
 	/* get root sectors and read each root directory entity */
@@ -141,12 +157,27 @@ void print_blocks_allocated(char* filename) {
 			}
 			mde = (struct msdos_dir_entry *) entity;
 			if(entity[0] == 0)
-				break;
+				continue;
 			/* name of the entity*/ 
 			unsigned char* name = mde->name;
 			char *namec = (char*)name;
-			printf("name:%s \n",namec);
-			if(!strcmp(namec, filename)) {
+			char *token = strtok(namec," ");
+			char* cfilename = malloc(sizeof(token));
+			char* extension;
+			strcpy(cfilename,token);
+			if(token!=NULL) {
+				token = strtok(NULL," ");
+				extension = malloc(sizeof(token));
+				strcpy(extension,token);
+			}
+
+			char* name_with_extension;
+			char dot[] =".";
+			name_with_extension = malloc(strlen(cfilename)+2+strlen(extension)); 
+			strcpy(name_with_extension, cfilename); 
+			strcat(name_with_extension, dot); 
+			strcat(name_with_extension, extension);
+			if(!strcasecmp(name_with_extension, filename)) {
 				/* First cluster low bytes */
 				unsigned int cluster_low = mde->start;
 				/* First cluster high bytes*/
@@ -202,12 +233,10 @@ void print_blocks_allocated(char* filename) {
 					while(cluster_no != 268435455) ;
 					
 					printf("%d)Cluster no:%d\n",count,cluster_no);
-					printf("The numbers of the clusters allocated to file %s is %d\n", name,count);
-					break;
+					printf("The numbers of the clusters allocated to file %s is %d\n", name_with_extension,count);
+					
 				}
-				else{
-					printf("The numbers of the clusters allocated to file %s is %d\n", name,count);
-				}
+				break;
 				
 			}
 			
@@ -250,12 +279,33 @@ void delete_file(char* filename) {
 			}
 			mde = (struct msdos_dir_entry *) entity;
 			if(entity[0] == 0)
-				break;
+				continue;
 			/* name of the entity*/ 
 			unsigned char* name = mde->name;
 			char *namec = (char*)name;
-			printf("name:%s \n",namec);
-			if(!strcmp(namec, filename)) {
+			char *token = strtok(namec," ");
+			char* cfilename = malloc(sizeof(token));
+			char* extension;
+			strcpy(cfilename,token);
+			if(token!=NULL) {
+				token = strtok(NULL," ");
+				extension = malloc(sizeof(token));
+				strcpy(extension,token);
+			}
+
+			char* name_with_extension;
+			char dot[] =".";
+			name_with_extension = malloc(strlen(cfilename)+2+strlen(extension)); 
+			strcpy(name_with_extension, cfilename); 
+			strcat(name_with_extension, dot); 
+			strcat(name_with_extension, extension);
+			
+			if(!strcasecmp(name_with_extension, filename)) {
+				/*remove the file from the directory*/
+				for(k = 0; k < DIRECTORYENTITYSIZE; k++) {
+					sector[j*DIRECTORYENTITYSIZE+k] = 0;
+				}
+				set_sector(sector,i);
 				/* First cluster low bytes */
 				unsigned int cluster_low = mde->start;
 				/* First cluster high bytes*/
@@ -277,7 +327,6 @@ void delete_file(char* filename) {
 					/* lookup FAT by comparing both for inconsistency*/
 					do {
 
-						printf("%d)Cluster no:%d\n",count,cluster_no);
 						/* get the starting file sector*/
 						int start_sect_no1 = 32 + (cluster_no*4) / SECTORSIZE;
 						int start_sect_no2 = 32 + sectors_per_fat_length + (cluster_no*4) / SECTORSIZE;
@@ -287,10 +336,21 @@ void delete_file(char* filename) {
 						get_sector(fat1Sector, start_sect_no1);
 						get_sector(fat2Sector, start_sect_no2);
 						int start_offset = (cluster_no*4) % SECTORSIZE;
+						
 						int firstlowbyte = fat1Sector[start_offset];
-						int secondlowbyte =fat1Sector[start_offset+1];
-						int thirdlowbyte =fat1Sector[start_offset+2];
-						int highestbyte =fat1Sector[start_offset+3];
+						int secondlowbyte = fat1Sector[start_offset+1];
+						int thirdlowbyte = fat1Sector[start_offset+2];
+						int highestbyte = fat1Sector[start_offset+3];
+						fat1Sector[start_offset] = 0;
+						fat1Sector[start_offset+1] = 0;
+						fat1Sector[start_offset+2] = 0;
+						fat1Sector[start_offset+3] = 0;
+						fat2Sector[start_offset] = 0;
+						fat2Sector[start_offset+1] = 0;
+						fat2Sector[start_offset+2] = 0;
+						fat2Sector[start_offset+3] = 0;
+						set_sector(fat1Sector,start_sect_no1);
+						set_sector(fat2Sector,start_sect_no2);
 						x = firstlowbyte;
 						y = secondlowbyte;
 						int z = thirdlowbyte;
@@ -306,29 +366,32 @@ void delete_file(char* filename) {
 						int merge;
 						sscanf(res,"%x",&merge);
 						cluster_no = merge;
-						count++;
 					}
 					while(cluster_no != 268435455) ;
 					
-					printf("%d)Cluster no:%d\n",count,cluster_no);
-					printf("The numbers of the clusters allocated to file %s is %d\n", name,count);
-					break;
 				}
-				else{
-					printf("The numbers of the clusters allocated to file %s is %d\n", name,count);
-				}
+				printf("File %s successfully deleted\n", name_with_extension);
+
+				break;
 				
 			}
+			
 			
 		}
 	}
 }
 
-void print_volume_sector() {
+void print_volume_info() {
 	get_sector (volumesector, 0); 
-	printf ("volume sector retrieved\n"); 
-	print_sector(volumesector); 
+	struct fat_boot_sector *bsp; 
+	bsp = (struct fat_boot_sector *) volumesector;
+	
+	/* read the number of sectors per fat at byte 36*/
+	unsigned int sectors_per_fat_length = bsp->fat32.length;
+	
 }
+
+
 
 int main(int argc, char *argv[])
 {
@@ -345,10 +408,28 @@ int main(int argc, char *argv[])
 		printf ("could not open the disk image\n"); 
 		exit (1); 
 	}
-	get_sector(volumesector,0);
+	char choice[40];
+	strcpy (choice, argv[2]);
+	char choice2[40];
+	strcpy (choice2, argv[3]);
+
+	if(strcmp(choice,"-p")) {
+		if(strcmp(choice2,"volumeinfo")) {
+			print_volume_info();
+		}
+		else if(strcmp(choice2,"rootdir")) {
+			print_rootdir();
+		}
+		else if(strcmp(choice2,"blocks")) {
+			char choice3[40]; 
+			strcpy (choice3, argv[4]);
+			print_blocks_allocated(choice3);
+		}
+	}
+	else if(strcmp(choice,"-d")) {
+		delete_file(choice2);
+	}
 	
-	print_rootdir();
-	print_blocks_allocated("EAZ03   BIN ");
 	close (disk_fd); 
 
 	return (0); 
